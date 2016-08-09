def053e9e5c455acff0026fcf040f248f62dc06e
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-08-10 00:09:35 +0200

    add naive implementation

---
+++ index.ts
@@ -0,0 +1,22 @@
+import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {isBlank} from '@angular/common/src/facade/lang';
+
+@Directive({ selector: '[src]' })
+export class SafeImg {
+  private _prevCondition: boolean = null;
+
+  constructor(private _viewContainer: ViewContainerRef, private _templateRef: TemplateRef<Object>) {
+  }
+
+  @Input()
+  set src(newCondition: any) {
+    if (newCondition && (isBlank(this._prevCondition) || !this._prevCondition)) {
+      this._prevCondition = true;
+      const res = this._viewContainer.createEmbeddedView(this._templateRef);
+      res.rootNodes[0].src = newCondition;
+    } else if (!newCondition && (isBlank(this._prevCondition) || this._prevCondition)) {
+      this._prevCondition = false;
+      this._viewContainer.clear();
+    }
+  }
+}
