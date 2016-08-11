30bf73c82d97ec4001c908c3fd6cba881ac55e8e
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-08-11 14:40:31 +0200

    start using chrome for travis tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -76,7 +76,7 @@ module.exports = function(config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-      'Firefox'
+      'Chrome'
     ],
 
     /*
